3ddc4ef639c7e3c9d6d484eb6c6dd9b4ff549884
Tenon <philipsamueladiele@gmail.com>
2024-02-19 20:49:39 +0100

    YT clone

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      },
+      colors: {
+        'trans': '#aaaaaa54',
+        'trans': '#aaaaaa54',
+      },
+      screens: {
+        'sm': '576px', 
+        'md': '768px', 
+        'lg': '992px', 
+        'xl': '1400px',
+      }
+    },
+  },
+  plugins: [],
+}
+
